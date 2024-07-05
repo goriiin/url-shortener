d62c744cab61fae847b099aecfd9ba2cf4949e5a
goriiin <afk00.17@bk.ru>
2024-07-05 20:32:43 +0300

    init go

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -1,1 +1,24 @@
 package main
+
+import (
+	"github.com/goriiin/myapp/backend/internal/config"
+	"log/slog"
+)
+
+func main() {
+	cfg := config.MustLoad()
+
+	log := config.SetupLogger(cfg.Env)
+
+	log.Info("starting server", slog.String("key", cfg.Env))
+	log.Debug("Debug mode enabled")
+
+	// TODO: init logger: slog
+
+	// TODO: init storage: Postgres
+
+	// TODO: init router: standard, render
+	path := "http://" + cfg.HTTPServer.Address + ":" + cfg.HTTPServer.Port
+	log.Info("starting server", slog.String("key", cfg.Env), slog.String("path", path))
+	// TODO: run server
+}
