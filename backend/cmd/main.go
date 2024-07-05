5d98b53c574edf0370c99167d157bc72a86cb903
goriiin <afk00.17@bk.ru>
2024-07-05 23:09:56 +0300

    update main

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"github.com/goriiin/myapp/backend/internal/config"
+	"github.com/goriiin/myapp/backend/internal/libs/sl"
+	"github.com/goriiin/myapp/backend/internal/storage/postgres"
 	"log/slog"
+	"os"
 )
 
 func main() {
@@ -13,9 +16,17 @@ func main() {
 	log.Info("starting server", slog.String("key", cfg.Env))
 	log.Debug("Debug mode enabled")
 
-	// TODO: init logger: slog
+	storage, err := postgres.New()
+	if err != nil {
+		log.Error("error creating postgres storage", sl.Err(err))
+		os.Exit(1)
+	}
+	//err = storage.RmTables()
+	//if err != nil {
+	//	log.Error("error removing tables", sl.Err(err))
+	//}
 
-	// TODO: init storage: Postgres
+	_ = storage
 
 	// TODO: init router: standard, render
 	path := "http://" + cfg.HTTPServer.Address + ":" + cfg.HTTPServer.Port
