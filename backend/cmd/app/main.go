1ea6f155aa100ca97151c232e64e20b87ae10bd6
goriiin <afk00.17@bk.ru>
2024-08-04 00:33:57 +0300

    server handlers CRUD

--- backend/cmd/app/main.go
+++ backend/cmd/app/main.go
@@ -1,11 +1,11 @@
 package main
 
 import (
-	config2 "github.com/goriiin/myapp/backend/db/postgres"
 	"github.com/goriiin/myapp/backend/internal/app/handlers"
 	"github.com/goriiin/myapp/backend/internal/app/middleware"
 	"github.com/goriiin/myapp/backend/internal/config"
-	service "github.com/goriiin/myapp/backend/internal/service "
+	"github.com/goriiin/myapp/backend/internal/repository/postgres"
+	service "github.com/goriiin/myapp/backend/internal/service"
 	"github.com/goriiin/myapp/backend/pkg/sl"
 	"log/slog"
 	"net/http"
@@ -15,50 +15,30 @@ import (
 func main() {
 	cfg := config.MustLoad()
 
-	// TODO: pretty logger?
 	log := config.SetupLogger(cfg.Env)
 
 	log.Info("starting server", slog.String("key", cfg.Env))
 	log.Debug("Debug mode enabled")
 
-	storage, err := config2.New()
-	if err != nil {
-		log.Error("error creating postgres repository", sl.Err(err))
-		os.Exit(1)
-	}
-	//err = repository.RmTables()
-	//if err != nil {
-	//	log.Error("error removing tables", sl.Err(err))
-	//}
+	storage := postgres.New()
 
-	serv = service.NewUrlSaverService(storage)
+	serv := service.NewUrlSaverService(storage)
 
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("/url", func(w http.ResponseWriter, r *http.Request) {
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		switch r.Method {
 		case http.MethodGet:
-			handlers.New(log)
+			handlers.Get(log, serv)(w, r)
 		case http.MethodPost:
-
+			handlers.Save(log, serv)(w, r)
 		case http.MethodPut:
-
+			handlers.Edit(log, serv)(w, r)
 		case http.MethodDelete:
-
+			handlers.Delete(log, serv)(w, r)
 		}
 	})
-	//mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-	//	_, _ = fmt.Fprintf(w, "Welcome to the homepage!")
-	//})
-	//
-	//mux.HandleFunc("/redirect", func(w http.ResponseWriter, r *http.Request) {
-	//	// Перенаправляем запрос на страницу профиля пользователя
-	//	http.Redirect(w, r, "/profile", http.StatusFound)
-	//})
-	//
-	//mux.HandleFunc("/profile", func(w http.ResponseWriter, r *http.Request) {
-	//	_, _ = fmt.Fprintf(w, "This is your profile page!")
-	//})
+
 	loggerMiddleware := middleware.New(log)
 	middlewareRec := middleware.NewRec(log)
 	_ = middlewareRec
@@ -74,7 +54,7 @@ func main() {
 	path := "http://" + cfg.HTTPServer.Address + ":" + cfg.HTTPServer.Port
 	log.Info("starting server", slog.String("key", cfg.Env), slog.String("path", path))
 
-	err = srv.ListenAndServe()
+	err := srv.ListenAndServe()
 	if err != nil {
 		log.Error("error starting server", sl.Err(err))
 		os.Exit(1)
