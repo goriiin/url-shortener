589c3d475d093baabb0a096ce4ecb2583d523ada
goriiin <afk00.17@bk.ru>
2024-08-04 02:31:43 +0300

    CRUD

--- backend/internal/app/handlers/url_saver.go
+++ backend/internal/app/handlers/url_saver.go
@@ -84,13 +84,62 @@ func Edit(log *slog.Logger, serv service.URLSaver) http.HandlerFunc {
 		log = log.With(
 			slog.String("op", op),
 		)
+		var req service.Url
 
+		err := json.NewDecoder(r.Body).Decode(&req)
+
+		fmt.Println(req)
+
+		if err != nil {
+			log.Error("fail to decode request body", sl.Err(err))
+			w.Header().Set("Content-Type", "application/json")
+
+			_ = json.NewEncoder(w).Encode(resp.Error("fail to decode"))
+
+			return
+		}
+
+		log.Info("request body decoded")
+
+		// валидация обязательного поля
+		if err = validator.New().Struct(req); err != nil {
+			validationErrors := err.(validator.ValidationErrors)
+			log.Error("fail to validate request body", sl.Err(err))
+
+			w.Header().Set("Content-Type", "application/json")
+			_ = json.NewEncoder(w).Encode(resp.ValidationError(validationErrors))
+
+			return
+		}
+
+		if newAlias, err := serv.EditURL(req.Url, req.Alias); err != nil {
+			if newAlias != nil {
+				req.Alias = *newAlias
+			} else {
+				log.Error("fail to decode request body", sl.Err(err))
+				w.Header().Set("Content-Type", "application/json")
+				_ = json.NewEncoder(w).Encode(resp.Error("fail to decode"))
+
+				return
+			}
+
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		_ = json.NewEncoder(w).Encode(Response{
+			Response: *resp.OK(),
+			URL:      req.Url,
+			Alias:    req.Alias,
+		})
 	}
 }
 
 func Delete(log *slog.Logger, serv service.URLSaver) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		const op = "handler.handlers.delete"
+
+		log = log.With(
+			slog.String("op", op))
 
 		query := r.URL.Query()
 		url := query.Get("url")
